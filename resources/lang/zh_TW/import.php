8fca24cfe9d5adf76d414ec593b23f75b39ca78c
Tim <lintaitin@gmail.com>
2024-09-10 17:28:32 +0800

    上傳檔案

--- resources/lang/zh_TW/import.php
+++ resources/lang/zh_TW/import.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'Upload_files' => '上傳檔案',
-    'Select File' => '請選擇匯入檔案',
+    'Upload_files' => '上傳檔案(圖檔或Zip檔)',
+    'Select File' => '請選擇匯入檔案(Excel資料檔)',
     'Import_success' => '匯入成功',
     'Download example' => '下載範本',
     'No' => '第',
